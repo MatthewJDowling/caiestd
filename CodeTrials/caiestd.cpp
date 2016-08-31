cab899c038d8a7a2414b3a977a4228856e0d2cf1
chelsey.lotts <chellybellie@gmail.com>
2016-08-31 11:10:51 -0700

    devideandconquer

--- CodeTrials/caiestd.cpp
+++ CodeTrials/caiestd.cpp
@@ -1,4 +1,5 @@
 #include "caiestd.h"
+#include <iostream>
 
 Point2D pointAdd(const Point2D & lhs, const Point2D & rhs)
 {
@@ -10,9 +11,28 @@ Point2D pointSub(const Point2D & lhs, const Point2D & rhs)
     return Point2D();
 }
 
+
+
+
+
+// For every 2 values, print the larger of the two.
+// Repeat this process until only one integer remains.
+//
+// Returns the last remaining integer.
 int divideAndConquer(int nums[], size_t numSize)
 {
-    return 0;
+	int pairCount = numSize / 2;
+	int index;
+	for ( int pair = 0; pair < pairCount; ++pair )
+	{
+		index = pair * 2;
+		if (nums[index] > nums[index + 1])
+			printf("%d", nums[index]);
+		else
+			printf("%d", nums[index + 1]);
+	}
+
+    return (index + 1);
 }
 
 int displacementOverlap(int aMin, int aMax, int bMin, int bMax)
