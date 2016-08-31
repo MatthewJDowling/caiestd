67d2467847a7efcbc65ad62f602dd2a93c1df2b2
matthew.dowling <matthew.dowling@SEA-8D0A81>
2016-08-31 10:38:05 -0700

    Put More Names In

--- CodeTrials/caiestd.cpp
+++ CodeTrials/caiestd.cpp
@@ -2,27 +2,27 @@
 #include "caiestd.h"
 #include <math.h>
 #include <cmath>
-
+//Josh
 Point2D pointAdd(const Point2D & lhs, const Point2D & rhs)
 {
     return Point2D();
 }
-
+//Pearson
 Point2D pointSub(const Point2D & lhs, const Point2D & rhs)
 {
     return Point2D();
 }
-
+//Chelsey
 int divideAndConquer(int nums[], size_t numSize)
 {
     return 0;
 }
-
+//Open
 int displacementOverlap(int aMin, int aMax, int bMin, int bMax)
 {
     return 0;
 }
-//mine
+//Ginger
 float degToRad(float deg)
 {
 	scanf_s("%d", deg);
@@ -31,46 +31,51 @@ float degToRad(float deg)
 	printf("%d", rad);
     return 0.0f;
 }
-
+//Xavier
 float radToDeg(float rad)
 {
     return 0.0f;
 }
 
+
+// Trevor
 int power(int base, int power)
 {
 	int ret;
 	ret = pow(base, power);
     return ret;
 }
-
+//Kobey
 float distance(Point2D first, Point2D second)
 {
     return 0.0f;
 }
 
+//Keil
 int sum(int nums[], size_t numSize)
 {
     return 0;
 }
 
+//Keil
 void fiboMyArray(int dest[], size_t destSize)
 {
 }
 
+//Open
 void concatIntArray(int srcA[], size_t srcSizeA, int srcB[], size_t srcSizeB, int dest[], size_t destSizeB)
 {
 }
-
+//Open
 Hotdog & applyHotdog(Hotdog & targetDog, int relishApps, int mustardApps, int creamCheeseaApps)
 {
     return Hotdog();
 }
-
+//Open
 void printHotdog(const Hotdog & targetDog)
 {
 }
-
+//Open
 void cookHotdog(Hotdog & targetDog)
 {
 }
